a4964cee1ce494ed6776338be59b477400b5b62e
Greg Priday <greg@siteorigin.com>
2026-05-27 21:54:57 +0200

    fix(plugin-sdk): rename Permission imports to Capability after #9421

--- shared/types/plugin-sdk.ts
+++ shared/types/plugin-sdk.ts
@@ -30,8 +30,8 @@ export type {
   ViewContribution,
   ViewLocation,
   McpServerContribution,
-  PluginPermission,
-  BuiltInPluginPermission,
+  PluginCapability,
+  BuiltInPluginCapability,
   PluginActionContribution,
 } from "./plugin.js";
 
